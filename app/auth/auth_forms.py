4c6ed2f0a90c60c5969dac0d12a3efeb656b56ce
ryan papazoglou <r.papazoglou@gmail.com>
2020-05-30 21:20:55 -0400

    Registration

--- app/auth/auth_forms.py
+++ app/auth/auth_forms.py
@@ -1,9 +1,32 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+from app.models import Users
+
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First name', validators=[DataRequired()])
+    last_name = StringField('Last name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    team_name = StringField('Team name', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+    def validate_teamname(self, team_name):
+        user = Users.query.filter_by(team_name=team_name.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different team name.')
+
+    def validate_email(self, email):
+        user = Users.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')

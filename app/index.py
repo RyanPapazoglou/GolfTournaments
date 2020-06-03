149273e78dda109521fd39652447adb5765d69ea
ryan papazoglou <r.papazoglou@gmail.com>
2020-06-03 14:38:26 -0400

    leaderboard

--- app/index.py
+++ app/index.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template
 from flask_login import login_required
+from sqlalchemy import desc
+from app.models import Users
 
 bp = Blueprint("golf", __name__, url_prefix="/")
 
@@ -7,3 +9,9 @@ bp = Blueprint("golf", __name__, url_prefix="/")
 @bp.route("/", methods=["POST","GET"])
 def index():
     return render_template("index.html")
+
+@bp.route("/leaderboard", methods=["POST","GET"])
+@login_required
+def leaderboard():
+    players=Users.query.order_by(desc(Users.points)).all()
+    return render_template("leaderboard.html", players=players)

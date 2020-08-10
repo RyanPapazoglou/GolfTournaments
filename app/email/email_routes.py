149a3b2c771c3de86bb86e9ad9b8a5ca9830fb9c
ryan papazoglou <r.papazoglou@gmail.com>
2020-08-10 00:39:36 -0400

    Contact feature

---
+++ app/email/email_routes.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, current_app, render_template
+from flask_login import current_user
+
+from app.email.email import Email
+from app.email.email_forms import SendEmailForm
+
+bp = Blueprint("email", __name__, url_prefix="/email")
+
+
+@bp.route("/contact", methods=["POST", "GET"])
+def contact():
+    form = SendEmailForm()
+    if form.validate_on_submit():
+        html = "<p>" + form.body.data + "</p>"
+        Email.send_email(
+            subject="Message from: " + current_user.email,
+            sender=current_app.config["MAIL_USERNAME"],
+            recipients=[current_app.config["MAIL_USERNAME"]],
+            text_body=form.body.data,
+            html_body=html,
+        )
+        return render_template("contact.html", title="Contact admins", success=True)
+    return render_template("contact.html", title="Contact admins", form=form)
